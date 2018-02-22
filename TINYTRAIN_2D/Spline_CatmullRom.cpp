409477103c94e72abb35174bb3ff267d9c9e286d
toksn <toksn@yahoo.de>
2018-02-22 14:12:17 +0100

    supporting uniform, chordal, centripetal catmull-rom splines now

--- TINYTRAIN_2D/Spline_CatmullRom.cpp
+++ TINYTRAIN_2D/Spline_CatmullRom.cpp
@@ -8,6 +8,7 @@ namespace tgf
 		Spline_CatmullRom::Spline_CatmullRom()
 		{
 			m_pointsPerSegment = 20;
+			type_ = CatmullRomType::Centripetal;
 		}
 
 		Spline_CatmullRom::~Spline_CatmullRom()
@@ -27,29 +28,83 @@ namespace tgf
 				int pt = new_control_point_index - 2;
 				for (int i = 0; i <= m_pointsPerSegment; i++)
 				{
-					float u = (float)i / (float)m_pointsPerSegment;
+					if (type_ == CatmullRomType::Uniform)
+					{
+						float u = (float)i / (float)m_pointsPerSegment;
+						appendSplinePoint(interpolateUniform(u, m_controlPoints[pt - 1].position, m_controlPoints[pt].position, m_controlPoints[pt + 1].position, m_controlPoints[pt + 2].position));
+					}
+					else
+					{
+						// for this calculation, see Definition section of https://en.wikipedia.org/wiki/Centripetal_Catmull%E2%80%93Rom_spline
+						float* x = new float[4]{ m_controlPoints[pt - 1].position.x, m_controlPoints[pt].position.x, m_controlPoints[pt+1].position.x, m_controlPoints[pt+2].position.x };
+						float* y = new float[4]{ m_controlPoints[pt - 1].position.y, m_controlPoints[pt].position.y, m_controlPoints[pt+1].position.y, m_controlPoints[pt+2].position.y };
+						float* time = new float[4]{ 0, 1, 2, 3 };
 
-					appendSplinePoint(interpolate(u, m_controlPoints[pt - 1].position, m_controlPoints[pt].position, m_controlPoints[pt + 1].position, m_controlPoints[pt + 2].position));
+						float tstart = 1.0f;
+						float tend = 2.0f;
+
+						float total = 0.0f;
+						for (int j = 1; j < 4; j++) 
+						{
+							float dx = x[j] - x[j - 1];
+							float dy = y[j] - y[j - 1];
+							if (type_ == CatmullRomType::Centripetal)
+								total += pow(dx * dx + dy * dy, .25);
+							else //if(type_ == CatmullRomType::Chordal)
+								total += pow(dx * dx + dy * dy, .5);
+							time[j] = total;
+						}
+						tstart = time[1];
+						tend = time[2];
+
+						float u = tstart + (i * (tend - tstart)) / m_pointsPerSegment;
+						//float u = (float)i / (float)m_pointsPerSegment;
+						appendSplinePoint(interpolate(u, m_controlPoints[pt - 1].position, m_controlPoints[pt].position, m_controlPoints[pt + 1].position, m_controlPoints[pt + 2].position, time));
+						delete[] x;
+						delete[] y;
+						delete[] time;
+					}
 				}
 			}
 		}
 
-		sf::Vector2f Spline_CatmullRom::interpolate(float u, sf::Vector2f pt0, sf::Vector2f pt1, sf::Vector2f pt2, sf::Vector2f pt3)
+		sf::Vector2f Spline_CatmullRom::interpolateUniform(float u, sf::Vector2f pt0, sf::Vector2f pt1, sf::Vector2f pt2, sf::Vector2f pt3)
 		{
+			float u2 = u * u;
+			float u3 = u2 * u;
+
+			float f1 = -0.5 * u3 + 1.0 * u2 - 0.5 * u;
+			float f2 =  1.5 * u3 - 2.5 * u2				+ 1.0;
+			float f3 = -1.5 * u3 + 2.0 * u2 + 0.5 * u;
+			float f4 =  0.5 * u3 - 0.5 * u2;
+
 			sf::Vector2f point;
-			point.x = interpolate(u, pt0.x, pt1.x, pt2.x, pt3.x);
-			point.y = interpolate(u, pt0.y, pt1.y, pt2.y, pt3.y);
+			point.x = pt0.x * f1 + pt1.x * f2 + pt2.x * f3 + pt3.x * f4;
+			point.y = pt0.y * f1 + pt1.y * f2 + pt2.y * f3 + pt3.y * f4;
 			return point;
 		}
 
-		float Spline_CatmullRom::interpolate(float u, float pt0, float pt1, float pt2, float pt3)
+		// calculation is based on figure 3 of http://www.cemyuksel.com/research/catmullrom_param/catmullrom.pdf
+		// uniform time vector should be the same as interpolateUniform(..) but slower because of the additional calcs
+		// 
+		// param: float* time is a float array of 4 entries, memory managment outside, no checks will be done in this func!
+		sf::Vector2f Spline_CatmullRom::interpolate(float u, sf::Vector2f pt0, sf::Vector2f pt1, sf::Vector2f pt2, sf::Vector2f pt3, float* time)
 		{
-			float point = u*u*u*	((-1) * pt0 + 3 * pt1 - 3 * pt2 + pt3) / 2;
-			point += u * u*(2 * pt0 - 5 * pt1 + 4 * pt2 - pt3) / 2;
-			point += u * ((-1) * pt0 + pt2) / 2;
-			point += pt1;
+			float L01 = pt0.x * (time[1] - u) / (time[1] - time[0]) + pt1.x * (u - time[0]) / (time[1] - time[0]);
+			float L12 = pt1.x * (time[2] - u) / (time[2] - time[1]) + pt2.x * (u - time[1]) / (time[2] - time[1]);
+			float L23 = pt2.x * (time[3] - u) / (time[3] - time[2]) + pt3.x * (u - time[2]) / (time[3] - time[2]);
+			float L012 = L01 * (time[2] - u) / (time[2] - time[0]) + L12 * (u - time[0]) / (time[2] - time[0]);
+			float L123 = L12 * (time[3] - u) / (time[3] - time[1]) + L23 * (u - time[1]) / (time[3] - time[1]);
+			float C12 = L012 * (time[2] - u) / (time[2] - time[1]) + L123 * (u - time[1]) / (time[2] - time[1]);
 
-			return point;
+			float yL01 = pt0.y * (time[1] - u) / (time[1] - time[0]) + pt1.y * (u - time[0]) / (time[1] - time[0]);
+			float yL12 = pt1.y * (time[2] - u) / (time[2] - time[1]) + pt2.y * (u - time[1]) / (time[2] - time[1]);
+			float yL23 = pt2.y * (time[3] - u) / (time[3] - time[2]) + pt3.y * (u - time[2]) / (time[3] - time[2]);
+			float yL012 = yL01 * (time[2] - u) / (time[2] - time[0]) + yL12 * (u - time[0]) / (time[2] - time[0]);
+			float yL123 = yL12 * (time[3] - u) / (time[3] - time[1]) + yL23 * (u - time[1]) / (time[3] - time[1]);
+			float yC12 = yL012 * (time[2] - u) / (time[2] - time[1]) + yL123 * (u - time[1]) / (time[2] - time[1]);
+
+			return sf::Vector2f(C12, yC12);
 		}
 	}
 }
--- TINYTRAIN_2D/Spline_CatmullRom.h
+++ TINYTRAIN_2D/Spline_CatmullRom.h
@@ -8,15 +8,27 @@ namespace tgf
 		class Spline_CatmullRom : public Spline
 		{
 		public:
+			// catmull rom type
+			// see http://www.cemyuksel.com/research/catmullrom_param/catmullrom.pdf for more info
+			enum class CatmullRomType
+			{
+				Uniform,		// uniform is faster, can introduce self intersections
+				Chordal,		// chordal is more curvy, less intersections
+				Centripetal		// centripetal in between, no intersections
+			};
+
+		
 			Spline_CatmullRom();
 			~Spline_CatmullRom();
+
+			CatmullRomType type_;
 
 		protected:
 			// Inherited via Spline
 			virtual void onControlPointsAdded(int a_startindex = 0) override;
 
-			virtual sf::Vector2f interpolate(float u, sf::Vector2f pt0, sf::Vector2f pt1, sf::Vector2f pt2, sf::Vector2f pt3);
-			float interpolate(float u, float pt0, float pt1, float pt2, float pt3);
+			sf::Vector2f interpolateUniform(float u, sf::Vector2f pt0, sf::Vector2f pt1, sf::Vector2f pt2, sf::Vector2f pt3);
+			sf::Vector2f interpolate(float u, sf::Vector2f pt0, sf::Vector2f pt1, sf::Vector2f pt2, sf::Vector2f pt3, float* time);
 		};
 	}
 }
