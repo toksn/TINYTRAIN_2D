999a3c080850231da0e0b7f5c335c8652a46d09b
toksn <toksn@yahoo.de>
2018-05-08 17:18:34 +0200

    TextureAtlas: added example code to use a potential json parser implementation for reading the frames data

--- TINYTRAIN_2D/TextureAtlas.cpp
+++ TINYTRAIN_2D/TextureAtlas.cpp
@@ -1,4 +1,10 @@
 #include "TextureAtlas.h"
+
+//#include <string>
+//#include <fstream>
+//#include <streambuf>
+//#include "json_tgf.h"
+
 namespace tgf
 {
 	namespace utilities
@@ -21,6 +27,52 @@ namespace tgf
 				// json data may be empty -> create json path by chaning texture file extension to json
 				// todo: read out json data to fill the map
 				//texture_coords_[texture_name] = sf::IntRect(x, y, w, h);
+				/*std::ifstream t(jsondata_file_path);
+				std::string str;
+
+				t.seekg(0, std::ios::end);
+				str.reserve(t.tellg());
+				t.seekg(0, std::ios::beg);
+
+				str.assign((std::istreambuf_iterator<char>(t)),
+					std::istreambuf_iterator<char>());
+					
+				// static functions of a potential json lib
+				json::object bla = json::parseFromJsonString(str);
+				//std::string somestring = json::parseToJsonString(bla);
+				
+				auto frames = bla["frames"]->asArray();
+				if (frames != nullptr)
+				{
+					for (auto& data : frames->arraydata)
+					{
+						if (data->type == json::types::object)
+						{
+							auto name = data["filename"]->asString();
+							auto frame = data["frame"]->asObject();
+
+							if (name && frame)	// && name->type == json::types::string && frame->type == json::types::object)
+							{
+								std::string key_name = name->string;
+								sf::IntRect r;
+								auto x = frame["x"]->asInt();
+								auto y = frame["y"]->asInt();
+								auto w = frame["w"]->asInt();
+								auto h = frame["h"]->asInt();
+								if (x && y && w && h)
+								{
+									r.left = x.integer;
+									r.top = y.integer;
+									r.width = w.integer;
+									r.height = h.integer;
+								}
+
+								// add frame
+								texture_coords_[key_name] = r;
+							}
+						}
+					}
+				}*/
 
 				texture_coords_["road"] = sf::IntRect(64, 0, 64, 64);
 				texture_coords_["road_crossing-1"] = sf::IntRect(0, 192, 128, 128);
